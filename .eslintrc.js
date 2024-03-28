e61160ccb24a56af68ce7f43f19f8953be3a25e6
Jonne Koivisto <142801048+jonnekoi@users.noreply.github.com>
2024-03-28 12:28:19 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': ['google', 'eslint:recommended'],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'require-jsdoc': 0,
+  },
+};
