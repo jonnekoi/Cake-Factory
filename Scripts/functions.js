7d69c8054c17e309de5df7f611f222824f22449a
mirohi <m_hintikka@hotmail.com>
2024-05-09 18:51:43 +0300

    Add cart count fix

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,14 +1,14 @@
-'use strict';
-
 export const updateCartCount = () => {
   let cart = localStorage.getItem('cart');
-  cart = cart ? JSON.parse(cart) : [];
-  if (cart.length === 0) {
+  cart = cart ? JSON.parse(cart) : undefined;
+  if (cart === 0) {
     const cartCount = document.querySelector('.cartItemCount');
     cartCount.style.display = 'none';
   } else {
-    const count = cart.reduce((total, item) => total + item.quantity, 0);
-    document.querySelector('.cartItemCount').innerText = count;
+    if (cart !== undefined) {
+      const count = cart.reduce((total, item) => total + item.quantity, 0);
+      document.querySelector('.cartItemCount').innerText = count;
+    }
     document.querySelector('.cartItemCount').style.color = 'lightblue';
   }
 };
